3bd544d31f63bc8e3cfd3a8ea36979a9c345abb3
Benjamin Franzke <ben@bnf.dev>
2024-08-09 15:03:01 +0200

    [TASK] Avoid polluting TYPO3 global in JavaScript

--- Resources/Public/JavaScript/Main.js
+++ Resources/Public/JavaScript/Main.js
@@ -1,7 +1,4 @@
-var TYPO3 = TYPO3 || {};
-TYPO3.tea = {};
-
-TYPO3.tea.makeSortable = function (table) {
+function makeSortable(table) {
   var th = table.tHead,
     i;
   th && (th = th.rows[0]) && (th = th.cells);
@@ -11,12 +8,12 @@ TYPO3.tea.makeSortable = function (table) {
     (function (i) {
       var dir = 1;
       th[i].addEventListener('click', function () {
-        TYPO3.tea.sortTable(table, i, (dir = 1 - dir));
+        sortTable(table, i, (dir = 1 - dir));
       });
     })(i);
 };
 
-TYPO3.tea.sortTable = function (table, col, reverse) {
+function sortTable(table, col, reverse) {
   var tb = table.tBodies[0],
     tr = Array.prototype.slice.call(tb.rows, 0),
     i;
@@ -31,6 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var t = document.querySelectorAll('.tx-tea table'),
     i = t.length;
   while (--i >= 0) {
-    TYPO3.tea.makeSortable(t[i]);
+    makeSortable(t[i]);
   }
 });
